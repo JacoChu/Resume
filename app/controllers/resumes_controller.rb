96e1a1506bf3e39f1cb1cbe40d338c5362c4efb5
jaco <a0976673231@gmail.com>
2022-04-11 17:11:35 +0800

    update list

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,4 +1,27 @@
 class ResumesController < ApplicationController
+    def index
+        @resumes =Resume.all
+    end    
+
     def new
+        @resume = Resume.new
     end
+
+    def create
+        
+        
+        @resume = Resume.new(clean_params)
+
+        if @resume.save
+            redirect_to resumes_path, notice: "新增成功"
+    
+        else
+            render :new
+        end
+    end
+
+    private
+        def clean_params
+            params.require(:resume).permit(:title, :content, :status)
+        end
 end
