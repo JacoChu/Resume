9ad6a9bb9fc1713fdedecdef53fd8768dc380492
jaco <a0976673231@gmail.com>
2022-04-12 20:10:28 +0800

    User login

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,4 +1,6 @@
 class ResumesController < ApplicationController
+    before_action :find_resume, only: [:edit, :update, :show, :destory]
+
     def index
         @resumes =Resume.all
     end    
@@ -8,20 +10,47 @@ class ResumesController < ApplicationController
     end
 
     def create
-        
-        
         @resume = Resume.new(clean_params)
 
         if @resume.save
             redirect_to resumes_path, notice: "新增成功"
-    
         else
             render :new
         end
     end
 
+    def show
+        
+    end
+
+    def destroy
+        
+        @resume.destroy
+        redirect_to resumes_path, notice: "已刪除"
+    end
+
+    def edit
+        
+    end
+
+    def update
+        
+
+        if @resume.update(clean_params)
+            redirect_to resumes_path, notice: "更新成功"
+        else
+            render :edit
+        end
+    end
+
+end
     private
         def clean_params
             params.require(:resume).permit(:title, :content, :status)
         end
-end
+
+        def find_resume
+            @resume = Resume.find(params[:id])
+        end
+
+        
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+
   get '/resumes', to:'resumes#index'
   get '/resumes/new', to:'resumes#new'
   post '/resumes', to: 'resumes#create'
-  root 'pages#index'
+  get '/resumes/:id', to: 'resumes#show', as: 'resume'
+  delete '/resumes/:id', to: 'resumes#destroy'
+  get '/resumes/:id/edit', to: 'resumes#edit', as: "edit_resume"
+  patch 'resumes/:id', to: "resumes#update"
+  # post '/'. to: 'pages#index'
+
+  get "/users/sign_up", to: "users#new"
+
+  root 'resumes#index'
   
 end
